602ee0954d3a9e402bda7344a1a250f285976a1a
Ruslan Loboda <tanatcu.iuko@gmail.com>
2017-06-27 01:56:55 +0300

    completed Observer, added .gitignore, created all mvc files

---
+++ services/Model.js
@@ -0,0 +1,3 @@
+function Model() {
+	console.info(new Observer())
+}
--- services/Observer.js
+++ services/Observer.js
@@ -17,5 +17,19 @@ Observer.prototype.add = function (observer) {
 };
 
 Observer.prototype.remove = function (observer) {
+	for (var i = 0; i < this.observers.length; i++) {
+		if (this.observers[i] === observer) {
+			this.observers.splice(i, 1);
+			return;
+		}
+	}
 
+	throw new Error('could not find observer in list of observers');
+};
+
+Observer.prototype.notify = function (data) {
+	var observersSnapshot = this.observers.slice(0);
+	for (var i = 0; i < observersSnapshot.length; i++) {
+		observersSnapshot[i](data);
+	}
 };
